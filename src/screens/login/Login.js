e0584ab453d2ed5d8aed9406cbe95c9e5b079364
Rana Rashid <rashidrana092@gmail.com>
2024-04-19 09:52:10 +0500

    login screen ui completed

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -7,6 +7,11 @@ import strings from '../../constants/lang';
 import Header from '../../components/Header';
 import TextinputWithLable from '../../components/TextinputWithLable';
 import WrapperContainer from '../../components/WrapperContainer';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import commonStyles from '../../styles/commonStyles';
+import colors from '../../styles/colors';
+import BtnComp from '../../components/BtnComp';
+import {moderateScale} from '../../styles/responsiveSize';
 
 const Login = () => {
   // instead of declaring multiple states for mulitple things, i am declaring one state for all purposes
@@ -24,23 +29,57 @@ const Login = () => {
   return (
     <WrapperContainer isLoading={isLoading}>
       <Header />
-      <TextinputWithLable
-        label={strings.USERNAME_OR_EMAIL}
-        placeholder={strings.PLEASE_ENTER_YOUR_EMAIL}
-        value={email}
-        onChangeText={email => updateState({email})}
-      />
-      <TextinputWithLable
-        label={strings.PASSWORD}
-        placeholder={strings.PLEASE_ENTER_YOUR_PASSWORD}
-        value={password}
-        onChangeText={password => updateState({password})}
-        secureTextEntry={isSecure}
-        isSecure={isSecure}
-        onPressSecure={() => updateState({isSecure: !isSecure})}
-      />
+      <View style={{flex: 1, justifyContent: 'space-between'}}>
+        <View>
+          <TextinputWithLable
+            label={strings.USERNAME_OR_EMAIL}
+            placeholder={strings.PLEASE_ENTER_YOUR_EMAIL}
+            value={email}
+            onChangeText={email => updateState({email})}
+          />
+          <TextinputWithLable
+            label={strings.PASSWORD}
+            placeholder={strings.PLEASE_ENTER_YOUR_PASSWORD}
+            value={password}
+            onChangeText={password => updateState({password})}
+            secureTextEntry={isSecure}
+            isSecure={isSecure}
+            onPressSecure={() => updateState({isSecure: !isSecure})}
+          />
+
+          <TouchableOpacity style={{alignSelf: 'center'}}>
+            <Text style={styles.forgotTextStyle}>
+              {strings.FORGOT_YOUR_PASSWORD}
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <BtnComp
+          btnText={strings.LOGIN}
+          btnStyle={styles.btnStyle}
+          textStyle={styles.textStyle}
+        />
+      </View>
     </WrapperContainer>
   );
 };
 
 export default Login;
+
+const styles = StyleSheet.create({
+  forgotTextStyle: {
+    ...commonStyles.fontSize12,
+    color: colors.blue,
+  },
+  btnStyle: {
+    borderRadius: moderateScale(54),
+    height: moderateScale(48),
+    backgroundColor: colors.blackOpacity20,
+    marginHorizontal: moderateScale(34),
+  },
+  textStyle: {
+    ...commonStyles.fontSize14,
+    color: colors.white,
+    textTransform: 'none',
+  },
+});
